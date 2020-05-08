01ad0fb71ba4da2eb72d896a1defb0cb8000d955
陈永飞 <chenyongfei@bmsoft.com.cn>
2020-05-08 15:25:32 +0800

    添加typeorm链接mysql数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as APP_CONFIG from '@app/app.config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: APP_CONFIG.MYSQLDB.HOST,
+            port: APP_CONFIG.MYSQLDB.PORT,
+            username: APP_CONFIG.MYSQLDB.USER,
+            password: APP_CONFIG.MYSQLDB.PASSWORD,
+            database: APP_CONFIG.MYSQLDB.DB_NAME,
+            entities: [APP_CONFIG.APP.ROOT_PATH + '/**/*.model.js'],
+            synchronize: true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { environment, isDevMode } from '@app/app.environment';
+
+import * as APP_CONFIG from '@app/app.config';
+
+// 替换 console 为更统一友好的
+const { log, warn, info } = console;
+const color = c => isDevMode ? c : '';
+Object.assign(global.console, {
+    log: (...args) => log('[log]', '[nodepress]', ...args),
+    warn: (...args) => warn(color('\x1b[33m%s\x1b[0m'), '[warn]', '[nodepress]', ...args),
+    info: (...args) => info(color('\x1b[34m%s\x1b[0m'), '[info]', '[nodepress]', ...args),
+    error: (...args) => info(color('\x1b[31m%s\x1b[0m'), '[error]', '[nodepress]', ...args),
+});
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(APP_CONFIG.APP.PORT);
 }
-bootstrap();
+bootstrap().then(() => {
+    console.log(APP_CONFIG.APP.ROOT_PATH + '/**/*.model.ts');
+    console.info(`NodePress Run！port at ${APP_CONFIG.APP.PORT}, env: ${environment}`);
+});
